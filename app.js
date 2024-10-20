a9757513cab854d1fb90b462535bb68cc492cd01
draketamer <lordtamer@outlook.kr>
2024-10-21 08:54:52 +0900

    #2.2 Variables

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("hi");
+// console.log(54545454545);
+// console.log("라라라라");
+
+const a = 11;
+const b = 2;
+
+const myName = "nico";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("Hello " + myName );
