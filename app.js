72c603ffd537d0e95b051ddad1d82f468ab49a26
draketamer <lordtamer@outlook.kr>
2024-10-22 07:49:13 +0900

    #2.5 Arrays

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const amIFat = null;
-// null은 아무것도 없는것으로 채웠다는 의미
-console.log(amIFat);
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
 
-let something;
-// undefined는 메모리는 비어져 있는데 아무것도 채워지지 않았다는 의미
-console.log(something);
+const daysOfWeek = [mon, tue, wed, thu, fri, sat];
+
+// const nonsense = [1,2,"hello", false, null, true, undefined,"nico"]
+
+// console.log(daysOfWeek,nonsense)
+
+//Get Item from Array
+console.log(daysOfWeek[5]);
+console.log(daysOfWeek[-1]); //undefined
+console.log(daysOfWeek[0,2]); // 3번째만 출력
+console.log(daysOfWeek[1,3]); // 4번째만 출력
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek[6]);
+
+
