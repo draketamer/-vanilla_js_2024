300916c0e6b4db1a1ea0ba05068ec0fbb2082538
draketamer <lordtamer@outlook.kr>
2024-10-21 13:29:12 +0900

    #2.3 const and let

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 // console.log(54545454545);
 // console.log("라라라라");
 
-const a = 11;
+let a = 11;
 const b = 2;
 
-const myName = "nico";
+let myName = "nico";
+
+// 규칙은, 기본적으로 const를 쓰고 필요할 때만 let을 쓰되, var는 쓰지 말것
 
 console.log(a+b);
 console.log(a*b);
 console.log(a/b);
 console.log("Hello " + myName );
+
+
+myName = "nicolas"
+
+console.log("Hello " + myName );
+
