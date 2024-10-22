000c7d1131aefc703379eea5233c4eeb6d997fe8
draketamer <lordtamer@outlook.kr>
2024-10-23 08:14:39 +0900

    #2.14 Conditionals part Two

--- app.js
+++ app.js
@@ -6,4 +6,14 @@
 // console.log(age, parseInt(age));
 
 const age = parseInt(prompt("How old are you?"));
-console.log(age);
+
+// console.log(age);
+
+console.log(isNaN(age));
+
+if (isNaN(age)) {
+  console.log("please write a number."); // condition == true
+} else {
+  console.log(age);
+  console.log("Thank you for writing your age."); // condition == false
+}
