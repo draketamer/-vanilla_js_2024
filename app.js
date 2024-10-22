fd22201d01584291ddacfbadd8cac043338a07a5
draketamer <lordtamer@outlook.kr>
2024-10-22 13:23:04 +0900

    #2.10 Recap II

--- app.js
+++ app.js
@@ -19,17 +19,44 @@
 // plus(8, 60);
 // divide(98, 20);
 
-const player = {
-  name: "nico",
-  sayHello: function (otherPersonsname) {
-    console.log("Hello " + otherPersonsname + " nice to meet you!");
+// const player = {
+//   name: "nico",
+//   sayHello: function (otherPersonsname) {
+//     console.log("Hello " + otherPersonsname + " nice to meet you!");
+//   },
+// };
+
+// console.log(player.name);
+// player.sayHello("lynn");
+// player.sayHello("dal");
+
+// const days = [];
+// days.push("aaa");
+// console.log(days[0]);
+// console.log(console);
+
+// function plus(a, b) {
+//   console.log(a + b);
+// }
+
+// plus(5, 10);
+
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  multiple: function (a, b) {
+    console.log(a * b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
   },
 };
 
-console.log(player.name);
-player.sayHello("lynn");
-player.sayHello("dal");
-
-const days = [];
-days.push("aaa");
-console.log(days[0]);
+calculator.add(2, 4);
+calculator.minus(2, 4);
+calculator.multiple(2, 4);
+calculator.divide(2, 4);
