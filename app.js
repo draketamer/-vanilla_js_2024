f6cf5c43ade47d5576c72356e3ee6c48a6e1f6cb
draketamer <lordtamer@outlook.kr>
2024-10-23 08:02:13 +0900

    #2.13 Conditionals

--- app.js
+++ app.js
@@ -1,40 +1,9 @@
-const calculator = {
-  add: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  times: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  power: function (a, b) {
-    return a ** b;
-  },
-};
+// const age = prompt("How old are you?");
 
-// console.log(calculator.add(2, 4));
-// console.log(calculator.minus(2, 4));
-// console.log(calculator.times(2, 4));
-// console.log(calculator.divide(2, 4));
-// console.log(calculator.power(2, 4));
+// parseInt(age);
 
-const plusResult = calculator.add(2, 4);
-const minusResult = calculator.minus(plusResult, 3);
-const timesResult = calculator.times(10, minusResult);
-const divideResult = calculator.divide(timesResult, plusResult);
-const powerResult = calculator.power(divideResult, plusResult);
+// console.log(typeof age, typeof parseInt(age));
+// console.log(age, parseInt(age));
 
-console.log(powerResult);
-
-const age = 96;
-function calculateKrAge(ageOfForeigner) {
-  return ageOfForeigner + 2;
-}
-
-const krAge = calculateKrAge(age);
-
-console.log(krAge);
+const age = parseInt(prompt("How old are you?"));
+console.log(age);
