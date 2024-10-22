f9e709592a3591445bfdbe8157f3187d2bdc0708
draketamer <lordtamer@outlook.kr>
2024-10-23 08:31:14 +0900

    #2.15 Conditionals part Three

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-// const age = prompt("How old are you?");
-
-// parseInt(age);
-
-// console.log(typeof age, typeof parseInt(age));
-// console.log(age, parseInt(age));
-
 const age = parseInt(prompt("How old are you?"));
 
-// console.log(age);
-
-console.log(isNaN(age));
-
-if (isNaN(age)) {
-  console.log("please write a number."); // condition == true
+if (isNaN(age) || age < 0) {
+  // ||: or
+  console.log("please write a real positive number."); // condition == true
+} else if (age < 18) {
+  console.log("You are too young.");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can drink."); // &&: and
+} else if (age > 50 && age <= 80) {
+  console.log("Can you drink ?");
+} else if (age > 80) {
+  console.log("You can do whatever you want.");
 } else {
-  console.log(age);
-  console.log("Thank you for writing your age."); // condition == false
+  console.log("You are too old. Be care for your condition."); // condition == false
 }
