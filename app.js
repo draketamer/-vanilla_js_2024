caadfaa4fda9c270f3c5d34adde53ca906d89ddb
draketamer <lordtamer@outlook.kr>
2024-10-24 07:38:44 +0900

    #3.2 Searching For Elements

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const title = document.querySelector(".hello h1");
 
 console.log(title);
+
+// const hellos = document.getElementsByClassName("hello");
+
+// hellos.innerText = "Got you!";
+
+// console.log(hellos);
