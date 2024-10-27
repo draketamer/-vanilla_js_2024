a6a52d988eaa8a67568d30e6210c3980f19847d6
draketamer <lordtamer@outlook.kr>
2024-10-28 08:19:52 +0900

    #3.7 CSS in Javascript part Two

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  // if (h1.style.color === "blue") {
-  //   h1.style.color = "tomato";
-  // } else {
-  //   h1.style.color = "blue";
-  // }
-  const currentColor = h1.style.color;
-  let newColor;
-  if (currentColor === "blue") {
-    newColor = "tomato";
+  const clickedClass = "clicked";
+  if (h1.className === clickedClass) {
+    h1.className = "";
   } else {
-    newColor = "blue";
+    h1.className = clickedClass;
   }
-  h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
-// h1.onclick = handleTitleClick;
-
-// step1 Element를 찾아라
-// step2 event를 listen 해라
-// step3 그 event에 react 해라
