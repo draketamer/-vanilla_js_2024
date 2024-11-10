c1dd44789283377f9c34b3a02c48a1dd401091c0
draketamer <lordtamer@outlook.kr>
2024-11-11 08:21:32 +0900

    #4.0 Input Values

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-// const loginForm = document.getElementById("login-form");
+// const loginForm = document.querySelector("#login-form");
 // const loginInput = loginForm.querySelector("input");
 // const loginButton = loginForm.querySelector("button");
+// 아래와 동일한 표현임
 
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-  console.log("Hello", loginInput.value);
+  // console.dir(loginInput);
   // console.log("click!!!!");
+  console.log("hello", loginInput.value);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
