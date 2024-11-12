627d4899cf588d68a0b3a578507cf68bd79071b6
draketamer <lordtamer@outlook.kr>
2024-11-12 22:28:02 +0900

    #4.6 Loading Username

--- app.js
+++ app.js
@@ -3,15 +3,30 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSumit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
   // greeting.innerText = "Hello " + username;
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username} keep going`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSumit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+// console.log(savedUsername);
+if (savedUsername === null) {
+  //show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSumit);
+} else {
+  //show the greetings
+  paintGreetings(savedUsername);
+}
