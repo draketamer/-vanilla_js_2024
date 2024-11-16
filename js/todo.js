6a7566e46a0bf13c25e9f46bd0f45baa09a3e26d
draketamer <lordtamer@outlook.kr>
2024-11-17 00:04:12 +0900

    #7.2 Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -3,12 +3,22 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  //   console.log(event.target.parentElement.innerText);
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newToDo) {
   // console.log("I will paint", newToDo);
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newToDo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
   //   console.log(li);
   toDoList.appendChild(li);
 }
