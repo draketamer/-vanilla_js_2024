176e90c7d863bd9c75ecd0dd4c5465e0d0637384
draketamer <lordtamer@outlook.kr>
2024-11-17 19:40:57 +0900

    Last Challenge

--- js/greetings.js
+++ js/greetings.js
@@ -15,7 +15,7 @@ function onLoginSumit(event) {
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
+  greeting.innerText = `Hello, ${username}!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
