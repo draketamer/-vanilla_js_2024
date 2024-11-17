f3e61d5403d4843e0744cc0ce2dd5c6728542b9e
draketamer <lordtamer@outlook.kr>
2024-11-17 16:39:42 +0900

    #8.1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,20 @@
+const API_KEY = "744631d01726b159d629a3dd4b3e11bb";
+
 function onGeoOk(position) {
   //   console.log(position);
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live in", lat, lng);
+  const lon = position.coords.longitude;
+  //   console.log("You live in", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  //   console.log(url);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+    });
 }
 function onGeoError() {
   alert("Can't find you. No weather for you.");
